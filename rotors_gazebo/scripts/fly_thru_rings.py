c12525991fb3559f1b5d3805adc90e8e1ec77011
Xin <uynsn@student.kit.edu>
2019-05-16 00:08:12 +0200

    traj gen ok, next as ros client

--- rotors_gazebo/scripts/fly_thru_rings.py
+++ rotors_gazebo/scripts/fly_thru_rings.py
@@ -21,6 +21,21 @@ def diff_eq_time_trans(tau,x,u,t_trans):
 
 	xdot = vertcat(vel,acc,dacc_dt) * t_trans
 	return xdot
+# def diff_eq_time_trans(tau,x,u,t_trans):
+# 	vel_xy = x[3]
+# 	azm = x[4]
+# 	vel_z = x[5]
+
+# 	vel = vertcat(vel_xy*cos(azm),vel_xy*sin(azm),vel_z)
+# 	acc = vertcat(x[6],x[7],x[8])
+# 	dacc_dt = vertcat(u[0],u[1],u[2])
+# 	dvelxy_dt = acc[0]*cos(azm)+acc[1]*sin(azm)
+# 	acc_centr = acc[0]*sin(azm)+acc[1]*cos(azm)
+# 	dazm_dt = acc_centr / vel_xy
+# 	dvz_dt = acc[2]
+
+# 	xdot = vertcat(vel,dvelxy_dt,dazm_dt,dvz_dt,dacc_dt) * t_trans
+# 	return xdot
 def rk4(ode, h, t, x, u):
 	k1 = ode(t,x,u);
 	k2 = ode(t,x+h/2*k1,u);
@@ -38,7 +53,7 @@ if __name__=='__main__':
 
 	x0pos=[0,0,0]
 	x0vel=[0,0,0]
-	x0acc=[0,0,0]
+	x0acc=[0,0,3]
 
 
 	x1pos = [2.,0.,1.5]
@@ -46,19 +61,22 @@ if __name__=='__main__':
 	x1azm = [0]
 	x1vel_y = 0
 	x1vel_z = 0
-	ring1 = [x1pos,x1azm]
+	x1vel=[inf,0,0]
+	ring1 = [x1pos,x1azm,x1vel]
 
 	x2pos = [6.,4.,1.5]
 	x2azm = [PI/2]
-	ring2 = [x2pos,x2azm]
+	
 	x2vel_x = 0
 	x2vel_z = 0
+	x2vel=[0,inf,0]
+	ring2 = [x2pos,x2azm,x2vel]
 	rings = [ring1,ring2]
 	rings_cnt = len(rings)
 
 	xFpos = [7,6,0.3]
 	xFvel = [0,0,0]
-	xFacc = [0,0,0]
+	xFacc = [0,0,3]
 
 	N = 25
 	x=MX.sym('x',9)
@@ -110,6 +128,8 @@ if __name__=='__main__':
 	# this initial condition has the specified bound, 
 	# with only one possible solution
 	# x0(0)=1; x1(0)=0;
+#	lbw += x0pos + [0,-PI,0] + x0acc
+#	ubw += x0pos + [0,PI,0] + x0acc
 	lbw += x0pos + x0vel + x0acc
 	ubw += x0pos + x0vel + x0acc
 	#solver's guess
@@ -139,21 +159,41 @@ if __name__=='__main__':
 
 			Xk = MX.sym('X_' + str(k+1), 9)
 			w   += [Xk]
+			# terminal state
 			if k==(rings_cnt+1)*N-1:
+			#	lbw += xFpos + [0,0,0] + xFacc
+			#	ubw += xFpos + [0,0,0] + xFacc
 				lbw += xFpos + xFvel + xFacc
 				ubw += xFpos + xFvel + xFacc
+			# thru a ring	
 			elif k==(r+1)*N-1:
 				xrpos = rings[r][0]
-				azm = rings[r][1]
+			#	azm = rings[r][1]
 				xrvel_z = 0
-				lbw += xrpos + [-vel_constr,-vel_constr,xrvel_z, -acc_constr,-acc_constr,-acc_constr]
-				ubw += xrpos +  [vel_constr,vel_constr,xrvel_z, acc_constr,acc_constr,acc_constr]
+			#	lbw += xrpos + [0,azm,xrvel_z, -acc_constr,-acc_constr,-acc_constr]
+			#	ubw += xrpos +  [vel_constr,azm,xrvel_z, acc_constr,acc_constr,acc_constr]
+				if np.isinf(rings[r][2][0]):
+					#only go in x direction
+					lbw += xrpos + [-vel_constr,0,xrvel_z, -acc_constr,-acc_constr,-acc_constr]
+					ubw += xrpos +  [vel_constr,0,xrvel_z, acc_constr,acc_constr,acc_constr]
+				elif np.isinf(rings[r][2][1]):
+					#only go in y direction
+					#x1vel=[0,inf,0]
+					lbw += xrpos + [0,-vel_constr,xrvel_z, -acc_constr,-acc_constr,-acc_constr]
+					ubw += xrpos +  [0,vel_constr,xrvel_z, acc_constr,acc_constr,acc_constr]
+				else:
+					lbw += xrpos + [-vel_constr,-vel_constr,-vel_constr, -acc_constr,-acc_constr,-acc_constr]
+					ubw += xrpos +  [vel_constr,vel_constr,vel_constr, acc_constr,acc_constr,acc_constr]
+				
+
+			#	lbw += xrpos + [vel_constr,vel_constr,xrvel_z, -acc_constr,-acc_constr,-acc_constr]
+			#	ubw += xrpos +  [vel_constr,vel_constr,xrvel_z, acc_constr,acc_constr,acc_constr]
 #				g += [atan2(Xk[4],Xk[3])]
 #				lbg += azm
 #				ubg += azm
 			else:
-				lbw += [-inf,-inf,-inf, -vel_constr,-vel_constr,-vel_constr, -acc_constr,-acc_constr,-acc_constr]
-				ubw += [inf,inf,inf, vel_constr,vel_constr,vel_constr, acc_constr,acc_constr,acc_constr]
+				lbw += [-inf,-inf,-inf, 0,-inf,-vel_constr, -acc_constr,-acc_constr,-acc_constr]
+				ubw += [inf,inf,inf, vel_constr,inf,vel_constr, acc_constr,acc_constr,acc_constr]
 
 			if r == 0:
 				w0  += x0pos+[0,0,0, 0,0,0]
@@ -162,63 +202,6 @@ if __name__=='__main__':
 			g   += [Xk_end-Xk]
 			lbg += [0,0,0, 0,0,0, 0,0,0]
 			ubg += [0,0,0, 0,0,0, 0,0,0]
-
-			
-		# for k in range(N,2*N):
-		# 	Uk = MX.sym('U_' + str(k), 3)
-		# 	w   += [Uk]
-		# #	if k!=0:
-		# 	lbw += [-input_constr,-input_constr,-input_constr]
-		# 	ubw += [input_constr,input_constr,input_constr]
-		# #	else:
-		# #		lbw += [0.25*gravity*mass,0.25*gravity*mass,0.25*gravity*mass,0.25*gravity*mass]
-		# #		ubw += [0.25*gravity*mass,0.25*gravity*mass,0.25*gravity*mass,0.25*gravity*mass]
-		# 	w0  += [0,0,0]
-		# 	# function that combines the computation of diff eq and Lagr term
-		# 	Fk = F(x0=Xk, p=Uk, tf_interval=t_interval_2)
-		# 	Xk_end = Fk['xf']
-		# 	J=J+Fk['qf']
-
-		# 	Xk = MX.sym('X_' + str(k+1), 9)
-		# 	w   += [Xk]
-		# 	if k!=2*N-1:
-		# 		lbw += [-inf,-inf,-inf, -vel_constr,-vel_constr,-vel_constr, -acc_constr,-acc_constr,-acc_constr]
-		# 		ubw += [inf,inf,inf, vel_constr,vel_constr,vel_constr, acc_constr,acc_constr,acc_constr]
-		# 	else:
-		# 		lbw += x2pos + [x2vel_x,-vel_constr,x2vel_z, -acc_constr,-acc_constr,-acc_constr]
-		# 		ubw += x2pos +  [x2vel_x,vel_constr,x2vel_z, acc_constr,acc_constr,acc_constr]
-		# 	w0  += x1pos+[0,0,0, 0,0,0]
-		# 	g   += [Xk_end-Xk]
-		# 	lbg += [0,0,0, 0,0,0, 0,0,0]
-		# 	ubg += [0,0,0, 0,0,0, 0,0,0]
-		# for k in range(2*N,3*N):
-		# 	Uk = MX.sym('U_' + str(k), 3)
-		# 	w   += [Uk]
-		# 	if k!=3*N-1:
-		# 		lbw += [-input_constr,-input_constr,-input_constr]
-		# 		ubw += [input_constr,input_constr,input_constr]
-		# 	else:
-		# 		lbw += [0,0,0]
-		# 		ubw += [0,0,0]
-		# 	w0  += [0,0,0]
-		# 	# function that combines the computation of diff eq and Lagr term
-		# 	Fk = F(x0=Xk, p=Uk, tf_interval=t_interval_3)
-		# 	Xk_end = Fk['xf']
-		# 	J=J+Fk['qf']
-
-		# 	Xk = MX.sym('X_' + str(k+1), 9)
-		# 	w   += [Xk]
-		# 	if k!=3*N-1:
-		# 		lbw += [-inf,-inf,-inf, -vel_constr,-vel_constr,-vel_constr, -acc_constr,-acc_constr,-acc_constr]
-		# 		ubw += [inf,inf,inf, vel_constr,vel_constr,vel_constr, acc_constr,acc_constr,acc_constr]
-		# 	else:
-		# 		lbw += xFpos + xFvel + xFacc
-		# 		ubw += xFpos + xFvel + xFacc
-		# 	w0  += x2pos+[0,0,0, 0,0,0]
-		# 	g   += [Xk_end-Xk]
-		# 	lbg += [0,0,0, 0,0,0, 0,0,0]
-		# 	ubg += [0,0,0, 0,0,0, 0,0,0]
-#	
 	w += t_intervals
 	for r in range(rings_cnt+1):
 		lbw += [0.1]
@@ -261,6 +244,8 @@ if __name__=='__main__':
 	pos_x_opt = w_opt[0::12]
 	pos_y_opt = w_opt[1::12]
 	pos_z_opt = w_opt[2::12]
+#	vel_xy_opt = w_opt[3::12]
+#	azm_opt = w_opt[4::12]
 	vel_x_opt = w_opt[3::12]
 	vel_y_opt = w_opt[4::12]
 	vel_z_opt = w_opt[5::12]
@@ -270,10 +255,16 @@ if __name__=='__main__':
 	jerk_x_opt=[]
 	jerk_y_opt=[]
 	jerk_z_opt=[]
+#	vel_x_opt=[]
+#	vel_y_opt=[]
 	for k in range((rings_cnt+1)*N):
 		jerk_x_opt += [w_opt[9+k*12]]
 		jerk_y_opt += [w_opt[10+k*12]]
 		jerk_z_opt += [w_opt[11+k*12]]
+#	for k in range((rings_cnt+1)*N):
+#		vel_x_opt+=[vel_xy_opt[k]*cos(azm_opt[k])]
+#		vel_y_opt+=[vel_xy_opt[k]*sin(azm_opt[k])]
+
 #	jerk_x_opt = w_opt[9::12]
 #	jerk_y_opt = w_opt[10::12]
 #	jerk_z_opt = w_opt[11::12]
@@ -288,6 +279,7 @@ if __name__=='__main__':
 	pos_subplot.plot(tgrid, pos_z_opt, '--')
 	pos_subplot.set_xlabel('t')
 	pos_subplot.legend(['x','y','z'])
+	pos_subplot.grid()
 
 	vel_subplot = fig.add_subplot(412)
 	vel_subplot.plot(tgrid, vel_x_opt, '-')
@@ -295,17 +287,29 @@ if __name__=='__main__':
 	vel_subplot.plot(tgrid, vel_z_opt, '-')
 	vel_subplot.set_xlabel('t')
 	vel_subplot.legend(['vx','vy','vz'])
+	vel_subplot.grid()
+
 	ang_subplot = fig.add_subplot(413)
 	ang_subplot.plot(tgrid, acc_x_opt, '-')
 	ang_subplot.plot(tgrid, acc_y_opt, '-')
 	ang_subplot.plot(tgrid, acc_z_opt, '-')
 	ang_subplot.set_xlabel('t')
 	ang_subplot.legend(['ax','ay','az'])
+	ang_subplot.grid()
+
 	angr_subplot = fig.add_subplot(414)
 	angr_subplot.plot(tgridu, jerk_x_opt, '-')
 	angr_subplot.plot(tgridu, jerk_y_opt, '-')
 	angr_subplot.plot(tgridu, jerk_z_opt, '-')
 	angr_subplot.set_xlabel('t')
 	angr_subplot.legend(['jx','jy','jz'])
-	plt.grid()
+	angr_subplot.grid()
+	plt.show()
+
+	fig2 = plt.figure(2)
+	plt.clf()
+	traj_subplot = fig2.add_subplot(111, projection='3d')
+	traj_subplot.plot(pos_x_opt, pos_y_opt, pos_z_opt)
+	traj_subplot.set_xlabel('x')
+	traj_subplot.set_ylabel('y')
 	plt.show()
